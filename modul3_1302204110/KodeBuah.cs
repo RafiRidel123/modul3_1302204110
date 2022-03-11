d5022aebcbbad3f19cf2e12b964a65ecce78ba92
RafiRide123 <>
2022-03-12 04:29:54 +0700

    menambahkan class KodeBuah

---
+++ modul3_1302204110/KodeBuah.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul3_1302204110
+{
+    internal class KodeBuah
+    {
+    }
+}
